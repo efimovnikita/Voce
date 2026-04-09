1751c1c22f4af9029c70706189385c04e887ebbd
Nikita Efimov <maskedballmail@gmail.com>
2026-04-09 09:16:41 +0300

    Remove unnecessary margin from play icon

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -76,7 +76,7 @@ const Player = ({ isPlaying, isLoading, onPlayPause, onRewind, playbackRate, onS
             </svg>
           ) : (
             // Иконка Play
-            <svg className="w-10 h-10 ml-1.5" fill="currentColor" viewBox="0 0 20 20">
+            <svg className="w-10 h-10" fill="currentColor" viewBox="0 0 20 20">
               <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clipRule="evenodd" />
             </svg>
           )}
