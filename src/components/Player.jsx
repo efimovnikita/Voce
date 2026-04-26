19945e6061d1ac647e04dd5e3052fac729b783e8
maskedball <maskedballmail@gmail.com>
2026-04-26 11:44:43 +0300

    Update pause button SVG path for better alignment

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -98,7 +98,7 @@ const Player = ({
             </svg>
           ) : isPlaying ? (
             <svg className="w-8 h-8 sm:w-10 sm:h-10" fill="currentColor" viewBox="0 0 20 20">
-              <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM7 8a1 1 0 012 0v4a1 1 0 11-2 0V8zm5 0a1 1 0 012 0v4a1 1 0 11-2 0V8z" clipRule="evenodd" />
+              <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM6.5 8a1 1 0 012 0v4a1 1 0 11-2 0V8zm5 0a1 1 0 012 0v4a1 1 0 11-2 0V8z" clipRule="evenodd" />
             </svg>
           ) : (
             <svg className="w-8 h-8 sm:w-10 sm:h-10" fill="currentColor" viewBox="0 0 20 20">
